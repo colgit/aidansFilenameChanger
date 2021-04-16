3fddcb4de44c6a7200472edca10a891173efb0ac
Aidan colbert <col86@gmail.com>
2021-04-16 13:36:20 -0700

    file overwrite safety measure added make scrub_active_filenames.py abort when filenames_in_active.cvs file already exists unless overwrite is confirmed.

--- scrub_active_filenames.py
+++ scrub_active_filenames.py
@@ -1,26 +1,37 @@
-# Importing library
+# Importing libraries
 import csv
 import os
+import os.path
 from os import listdir
 from os.path import join, isfile
+NAMEOFFILE = 'filenames_in_active.cvs'
+NAMEOFDIR = 'active'
 print('getting path...')
 path = os.getcwd()
-print('path: %s',path)
-print('Directory to be scanned should be named \'active\' in %s',path)
+print('path: {0}'.format(path))
+print('Directory to be scanned should be named \'{0}\' in {1}'.format(NAMEOFDIR,path))
 # data to be written row-wise in csv file
-path_active = join(path,'active')
+path_active = join(path,NAMEOFDIR)
 if os.path.isdir(path_active):
     active_file_names = [f for f in listdir(path_active) if isfile(join(path_active,f))]
     # opening the csv file in 'w+' mode
-    file = open('filenames_in_active' +'.cvs', 'w+', newline ='')
+    if os.path.exists(NAMEOFFILE):
+        print('{0} already exists.'.format(NAMEOFFILE))
+        choice = input('Do you want to overwrite? Type y for yes...')
+        if (choice != 'y'):
+            print('Aborting...')
+            exit()
+    
+    file = open(NAMEOFFILE, 'w+', newline ='')
     print('Writing...')  
     # writing the data into the file
     with file:    
-        writer = csv.writer(file, dialect='excel', delimiter =",")
+        writer = csv.writer(file, dialect='excel', delimiter =',')
         writer.writerows([c.strip() for c in r.split(',')] for r in active_file_names)
-    print('Names of files in \'active\' directory output to file.')
-    file.close()
+        file.write('\n') #append newline at end of file.
+        
+        print('Names of files in \'{0}\' directory output to file.'.format(NAMEOFFILE))
+        file.close()
 else:
-    print('Files to be scanned should be located in a directory named \'active\' located in the current directory.')
-print('Exiting') 
+    print('Files to be scanned should be located in a directory named \'{0}\' located in the current directory. Aborting...'.format(NAMEOFDIR)) 
 exit()
